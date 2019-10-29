0d9001ccb2e9d1753c556536e6b9dc43419780e8
erdem3801 <burkaerdem@gmail.com>
2019-10-29 16:04:53 +0300

    HMI SCADA over TCP-IP

---
+++ Al-Ba/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Al_Ba
+{
+    class socket
+    {
+        public static string  Ip { get; set; }
+
+        public static int port { get; set; }
+    }
+    class server
+    {
+
+        public static string username { get; set; }// sql server kullanıcı adı
+
+        public static string servername { get; set; }// sql server adı
+
+        public static string password { get; set; }// sql server password
+
+        public static string dbname { get; set; }// database adı
+
+        public static string sql_port { get; set; }// database adı
+
+        public static string tbname { get; set; }// tablo adı
+    }
+    public class sqlpost
+    {
+        public string address { get; set; }
+
+        public string value { get; set; }
+
+        public string definition { get; set; }
+    }
+
+    public class listitems
+    {
+        public bool status { get; set; }
+
+        public int address { get; set; }
+
+        public string type { get; set; }
+
+        public string definition { get; set; }
+    }
+}
---
+++ Al-Ba/set.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace Al_Ba
+{
+    partial class set
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Addresss = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.definition = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.port = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.server_name = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dbname = new System.Windows.Forms.TextBox();
+            this.tbname = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.mailbox = new System.Windows.Forms.TextBox();
+            this.mailnotification = new System.Windows.Forms.CheckBox();
+            this.sql_port = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(278, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Address";
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(74, 90);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(69, 20);
+            this.address.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RW",
+            "LW",
+            "SRW",
+            "LB",
+            "SRB"});
+            this.comboBox1.Location = new System.Drawing.Point(149, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(123, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Addresss,
+            this.adres,
+            this.type,
+            this.value});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 182);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(593, 332);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_RowHeaderMouseDoubleClick);
+            // 
+            // Addresss
+            // 
+            this.Addresss.HeaderText = "Address";
+            this.Addresss.Name = "Addresss";
+            // 
+            // adres
+            // 
+            this.adres.HeaderText = "Type";
+            this.adres.Name = "adres";
+            this.adres.Width = 150;
+            // 
+            // type
+            // 
+            this.type.HeaderText = "Status";
+            this.type.Name = "type";
+            this.type.Width = 150;
+            // 
+            // value
+            // 
+            this.value.HeaderText = "Definition";
+            this.value.Name = "value";
+            this.value.Width = 160;
+            // 
+            // definition
+            // 
+            this.definition.Location = new System.Drawing.Point(74, 117);
+            this.definition.Name = "definition";
+            this.definition.Size = new System.Drawing.Size(198, 20);
+            this.definition.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Port:";
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(177, 15);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(48, 20);
+            this.port.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(231, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Check Connection";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(41, 14);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(95, 20);
+            this.IP.TabIndex = 3;
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(433, 12);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(132, 20);
+            this.user.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Definition";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(433, 38);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(132, 20);
+            this.password.TabIndex = 9;
+            // 
+            // server_name
+            // 
+            this.server_name.Location = new System.Drawing.Point(433, 64);
+            this.server_name.Name = "server_name";
+            this.server_name.Size = new System.Drawing.Size(85, 20);
+            this.server_name.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(395, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "User:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(374, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Password:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(358, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Server/Port:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(371, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Database:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(386, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Table:";
+            // 
+            // dbname
+            // 
+            this.dbname.Location = new System.Drawing.Point(433, 90);
+            this.dbname.Name = "dbname";
+            this.dbname.Size = new System.Drawing.Size(132, 20);
+            this.dbname.TabIndex = 9;
+            // 
+            // tbname
+            // 
+            this.tbname.Location = new System.Drawing.Point(433, 116);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(132, 20);
+            this.tbname.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(454, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 21);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(25, 64);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Mail to:";
+            // 
+            // mailbox
+            // 
+            this.mailbox.Location = new System.Drawing.Point(74, 60);
+            this.mailbox.Name = "mailbox";
+            this.mailbox.Size = new System.Drawing.Size(164, 20);
+            this.mailbox.TabIndex = 9;
+            this.mailbox.Leave += new System.EventHandler(this.Mailbox_Leave);
+            // 
+            // mailnotification
+            // 
+            this.mailnotification.AutoSize = true;
+            this.mailnotification.Location = new System.Drawing.Point(244, 60);
+            this.mailnotification.Name = "mailnotification";
+            this.mailnotification.Size = new System.Drawing.Size(98, 17);
+            this.mailnotification.TabIndex = 10;
+            this.mailnotification.Text = "mail notification";
+            this.mailnotification.UseVisualStyleBackColor = true;
+            this.mailnotification.CheckedChanged += new System.EventHandler(this.Mailnotification_CheckedChanged);
+            // 
+            // sql_port
+            // 
+            this.sql_port.Location = new System.Drawing.Point(519, 64);
+            this.sql_port.Name = "sql_port";
+            this.sql_port.Size = new System.Drawing.Size(46, 20);
+            this.sql_port.TabIndex = 9;
+            // 
+            // set
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.mailnotification);
+            this.Controls.Add(this.tbname);
+            this.Controls.Add(this.sql_port);
+            this.Controls.Add(this.server_name);
+            this.Controls.Add(this.dbname);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.mailbox);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.definition);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "set";
+            this.Size = new System.Drawing.Size(591, 514);
+            this.Load += new System.EventHandler(this.Set_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox definition;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Addresss;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn value;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        public System.Windows.Forms.TextBox port;
+        public System.Windows.Forms.TextBox IP;
+        public System.Windows.Forms.TextBox user;
+        public System.Windows.Forms.TextBox password;
+        public System.Windows.Forms.TextBox server_name;
+        public System.Windows.Forms.TextBox dbname;
+        public System.Windows.Forms.TextBox tbname;
+        private System.Windows.Forms.Label label10;
+        public System.Windows.Forms.TextBox mailbox;
+        public System.Windows.Forms.CheckBox mailnotification;
+        public System.Windows.Forms.TextBox sql_port;
+    }
+}
